136ba4a243c3700d60805b936fcbdb735d056a2b
Rofi <rofisudyono@gmail.com>
2022-08-26 07:05:54 +0700

    slicing page instance

--- src/components/molecules/Modal/index.tsx
+++ src/components/molecules/Modal/index.tsx
@@ -1,30 +1,41 @@
-import {Modal as ModalMaterialUi, Typography} from '@mui/material';
-import Image from 'next/image';
-import {ReactNode} from 'react';
-import {IcClosePurple} from '../../../../public/icons';
+import { Modal as ModalMaterialUi, Typography } from "@mui/material";
+import Image from "next/image";
+import { ReactNode } from "react";
+import { IcClosePurple } from "../../../../public/icons";
 
 interface ModalInterface {
   open: boolean;
   onClose: () => void;
   children: ReactNode;
   title: string;
+  type?: string;
 }
 const ModalComponent = (props: ModalInterface) => {
-  const {open, onClose, children, title} = props;
+  const { open, onClose, children, title, type } = props;
 
   return (
     <ModalMaterialUi
       open={open}
       onClose={onClose}
       aria-labelledby="modal-modal-title"
-      aria-describedby="modal-modal-description">
-      <div className="bg-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-lg w-[555px] p-[20px]">
+      aria-describedby="modal-modal-description"
+    >
+      <div
+        className={
+          type === "small"
+            ? "bg-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px] p-[20px]"
+            : "bg-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-lg w-[555px] p-[20px]"
+        }
+      >
         <div className="flex items-center justify-between">
           <div className="w-[20px] h-[20px]" />
           <Typography id="modal-modal-title" variant="h6" component="h2">
             {title}
           </Typography>
-          <button className="text-purple-700 w-[20px] h-[20px]" onClick={onClose}>
+          <button
+            className="text-purple-700 w-[20px] h-[20px]"
+            onClick={onClose}
+          >
             <Image src={IcClosePurple} height={20} width={20} alt="" />
           </button>
         </div>
