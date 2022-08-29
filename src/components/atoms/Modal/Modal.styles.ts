b9badeaeba4cfbbe949f6c2ee151ab6a14fd5d14
abdulhakim2902 <hakim@blocksphere.id>
2022-08-29 10:14:56 +0700

    feat: create new modal atom

---
+++ src/components/atoms/Modal/Modal.styles.ts
@@ -0,0 +1,78 @@
+import {createStyles, makeStyles, Theme} from '@material-ui/core/styles';
+
+import {AllignTitle, TitleSize} from './Modal.types';
+
+type StylesProps = {
+  align: AllignTitle;
+  titleSize: TitleSize;
+  gutter: 'none' | 'default' | 'custom';
+  fullScreen?: boolean;
+};
+
+export const useStyles = makeStyles<Theme, StylesProps>(theme =>
+  createStyles({
+    root: {
+      '& .MuiPaper-rounded': {
+        borderRadius: props => (props.fullScreen ? 0 : 10),
+      },
+    },
+    paper: {
+      [theme.breakpoints.down('xs')]: {
+        margin: props => (props.fullScreen ? 0 : 20),
+      },
+    },
+    nav: {
+      marginBottom: 20,
+    },
+    title: {
+      textAlign: props => props.align,
+      padding: 30,
+
+      [theme.breakpoints.down('xs')]: {
+        padding: 20,
+      },
+
+      '& .MuiTypography-h4': {
+        lineHeight: '30px',
+        fontWeight: props => (props.titleSize === 'small' ? 400 : 700),
+        color: '#000000',
+        marginBottom: theme.spacing(1),
+      },
+    },
+    subtitle: {
+      fontSize: '14px',
+    },
+    close: {
+      position: 'absolute',
+      right: 30,
+      top: 30,
+
+      [theme.breakpoints.down('xs')]: {
+        right: 20,
+        top: 20,
+      },
+
+      '& .MuiSvgIcon-colorPrimary': {
+        fill: 'currentColor',
+        color: 'none',
+      },
+    },
+    content: {
+      padding: props =>
+        props.gutter === 'default'
+          ? '0 30px 30px 30px'
+          : props.gutter === 'none'
+          ? 0
+          : '0 0 30px 0',
+
+      [theme.breakpoints.down('xs')]: {
+        padding: props =>
+          props.gutter === 'default'
+            ? '0 20px 20px 20px'
+            : props.gutter === 'none'
+            ? 0
+            : '0 0 20px 0',
+      },
+    },
+  }),
+);
---
+++ src/components/atoms/Modal/Modal.tsx
@@ -0,0 +1,77 @@
+import {XIcon} from '@heroicons/react/solid';
+
+import React from 'react';
+
+import {IconButton, SvgIcon, Typography} from '@material-ui/core';
+import Dialog, {DialogProps} from '@material-ui/core/Dialog';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+import {useStyles} from './Modal.styles';
+import {AllignTitle, TitleSize} from './Modal.types';
+
+import ShowIf from 'src/components/common/show-if.component';
+
+export type ModalProps = DialogProps & {
+  title?: string | React.ReactNode;
+  subtitle?: string | React.ReactNode;
+  children: React.ReactNode;
+  align?: AllignTitle;
+  titleSize?: TitleSize;
+  gutter?: 'none' | 'default' | 'custom';
+  onClose: () => void;
+};
+
+export const Modal: React.FC<ModalProps> = props => {
+  const {
+    title,
+    subtitle,
+    children,
+    onClose,
+    align = 'center',
+    titleSize = 'medium',
+    gutter = 'default',
+    className,
+    fullScreen,
+    ...otherProps
+  } = props;
+
+  const styles = useStyles({align, titleSize, gutter, fullScreen});
+
+  const handleClose = () => {
+    onClose();
+  };
+
+  return (
+    <Dialog
+      onClose={handleClose}
+      {...otherProps}
+      fullScreen={fullScreen}
+      className={styles.root}
+      classes={{paper: styles.paper}}
+      disableEnforceFocus>
+      <ShowIf condition={!fullScreen}>
+        <DialogTitle disableTypography className={[styles.title, className].join(' ')}>
+          <Typography variant={titleSize === 'small' ? 'h5' : 'h4'}>{title}</Typography>
+          {subtitle && (typeof subtitle === 'string' || subtitle instanceof String) ? (
+            <Typography variant="subtitle1" display="block" className={styles.subtitle}>
+              {subtitle}
+            </Typography>
+          ) : (
+            <>{subtitle}</>
+          )}
+          <IconButton
+            color="secondary"
+            aria-label="close"
+            size="small"
+            className={styles.close}
+            onClick={onClose}>
+            <SvgIcon component={XIcon} color="primary" fontSize="medium" />
+          </IconButton>
+        </DialogTitle>
+      </ShowIf>
+
+      <DialogContent className={styles.content}> {children} </DialogContent>
+    </Dialog>
+  );
+};
