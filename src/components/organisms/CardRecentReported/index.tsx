901f1c2cf07ecdc05f3264f1891408039ae737d7
abdulhakim2902 <hakim@blocksphere.id>
2022-09-01 19:17:43 +0700

    fix: fixed lint

--- src/components/organisms/CardRecentReported/index.tsx
+++ src/components/organisms/CardRecentReported/index.tsx
@@ -1,31 +1,31 @@
-import React from "react";
-import { AvatarWithName } from "src/components/atoms";
-import Button from "src/components/atoms/Button";
+import React from 'react';
+import {AvatarWithName} from 'src/components/atoms';
+import Button from 'src/components/atoms/Button';
 
-export default function CardRecentReported({ title }: { title: string }) {
+export default function CardRecentReported({title}: {title: string}) {
   return (
     <div className="col-span-1 p-5 bg-white shadow-lg rounded-2xl h-[320px]">
       <div className="text-lg font-semibold mb-4">{title}</div>
       <div className="grid grid-rows-3 gap-4">
         <AvatarWithName
-          image={"https://i.pravatar.cc/300"}
-          name={"Ronaldo"}
-          desc={"Reported a user"}
+          image={'https://i.pravatar.cc/300'}
+          name={'Ronaldo'}
+          desc={'Reported a user'}
         />
         <AvatarWithName
-          image={"https://i.pravatar.cc/300"}
-          name={"Ronaldo"}
-          desc={"Reported a user"}
+          image={'https://i.pravatar.cc/300'}
+          name={'Ronaldo'}
+          desc={'Reported a user'}
         />
         <AvatarWithName
-          image={"https://i.pravatar.cc/300"}
-          name={"Ronaldo"}
-          desc={"Reported a user"}
+          image={'https://i.pravatar.cc/300'}
+          name={'Ronaldo'}
+          desc={'Reported a user'}
         />
         <AvatarWithName
-          image={"https://i.pravatar.cc/300"}
-          name={"Ronaldo"}
-          desc={"Reported a user"}
+          image={'https://i.pravatar.cc/300'}
+          name={'Ronaldo'}
+          desc={'Reported a user'}
         />
       </div>
       <div className="w-full items-center justify-center flex">
