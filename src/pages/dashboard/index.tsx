b40a6f9603466dc4d0ccf579acd7ae5b439126c0
Rofi <rofisudyono@gmail.com>
2022-09-29 05:32:25 +0700

    integration chart post

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -109,7 +109,7 @@ export default function Dashboard() {
           <div className="col-span-1 p-5 bg-white shadow-lg rounded-2xl">
             <div className="text-lg font-semibold">Post Statistics</div>
             <div className="h-full w-full flex justify-center items-center">
-              <ChartDoughnat height={175} />
+              <ChartDoughnat height={175} data={selectedInstance?.detail?.metric.totalPosts} />
             </div>
           </div>
           <div className="col-span-1 p-5 bg-white shadow-lg rounded-2xl">
@@ -119,7 +119,7 @@ export default function Dashboard() {
           <div className="col-span-1 p-5 bg-white shadow-lg rounded-2xl">
             <div className="text-lg font-semibold">Wallet Statistics</div>
             <div className="h-full w-full flex justify-center items-center">
-              <ChartPie />
+              <ChartPie data={selectedInstance?.detail?.metric.totalPosts} />
             </div>
           </div>
         </div>
@@ -127,7 +127,7 @@ export default function Dashboard() {
           <div className="col-span-1 p-5 bg-white shadow-lg rounded-2xl mb-6">
             <div className="text-lg font-semibold mb-6">Connected Social Media Account</div>
             <div className="w-full flex items-center justify-center">
-              <ChartDoughnat height={380} />
+              <ChartDoughnat height={380} data={selectedInstance?.detail?.metric.totalPosts} />
             </div>
           </div>
           <div className="col-span-1 p-5 bg-white shadow-lg rounded-2xl mb-6">
