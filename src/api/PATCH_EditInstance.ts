86dc6642368bf37e4f05d97e503859b34f371ced
Rofi <rofisudyono@gmail.com>
2022-10-14 19:52:42 +0700

    fix: remove console.log

--- src/api/PATCH_EditInstance.ts
+++ src/api/PATCH_EditInstance.ts
@@ -9,11 +9,6 @@ interface BodyInterface {
   images: object;
 }
 
-let config = {
-  headers: {
-    Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyZmI4NzgwMTgyYmY2MDAxYjA5ZDYwOCIsIm5hbWUiOiJSb2ZpIiwidXNlcm5hbWUiOiJyb2ZpdGVzIiwiY3JlYXRlZEF0IjoiMjAyMi0wOC0xNlQxMjowMzoxMi41ODZaIiwicGVybWlzc2lvbnMiOlsidXNlciIsImFkbWluIl0sImlhdCI6MTY2NTcxNTkyMX0.XkzLk40qfe3S_1iN7s0DQWMg4Dml6JpqlXoYbEDx2Fs`,
-  },
-};
 export async function patchEditInstance({
   baseUrl,
   accessToken,
