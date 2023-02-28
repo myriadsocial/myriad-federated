e9eae51eadf3b5c451b6f05236467f7da4a7087a
nekaartajaya <nekaartajaya@gmail.com>
2023-02-28 15:59:39 +0800

    feat: switch network in modal staking reward

--- src/components/molecules/SwitchNetwork/index.tsx
+++ src/components/molecules/SwitchNetwork/index.tsx
@@ -1,18 +1,33 @@
 import { ChevronDownIcon } from '@heroicons/react/outline';
-import { Button, Menu, MenuItem, SvgIcon, Typography } from '@mui/material';
+import {
+  Backdrop,
+  Button,
+  CircularProgress,
+  Menu,
+  MenuItem,
+  SvgIcon,
+  Typography,
+} from '@mui/material';
 import Image from 'next/image';
 import { IcBackPrimary, IcDebio, IcMyriad } from 'public/icons';
 import React from 'react';
 import ListNetwork from 'src/components/atoms/ListNetwork';
 import ListWallet from 'src/components/atoms/ListWallet';
+import { ShimerComponent } from 'src/components/organisms/ServerList/Shimer';
+import { InstanceType, useInstances } from 'src/hooks/use-instances.hook';
+import { ServerListProps } from 'src/interface/ServerListInterface';
+import ShowIf from '../common/show-if.component';
 
 export type NetworkOptionProps = {
-  handleSelect: (network: any) => void;
+  onChangeNetwork?: (network: string) => Promise<void>;
 };
 
-export const SwitchNetwork: React.FC<NetworkOptionProps> = ({
-  handleSelect,
-}) => {
+export const SwitchNetwork: React.FC<NetworkOptionProps> = (props) => {
+  const { onChangeNetwork } = props;
+  const { currentNetworkId, setCurrentNetworkId } = useInstances(
+    InstanceType.OWNED,
+  );
+
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
 
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
@@ -21,6 +36,12 @@ export const SwitchNetwork: React.FC<NetworkOptionProps> = ({
 
   const handleClose = () => {
     setAnchorEl(null);
+  };
+
+  const handleChangeNetwork = (networkId: string) => {
+    onChangeNetwork?.(networkId);
+    setCurrentNetworkId(networkId);
+    handleClose();
   };
 
   return (
@@ -39,7 +60,9 @@ export const SwitchNetwork: React.FC<NetworkOptionProps> = ({
         size="small"
         color="inherit"
       >
-        <div className="text-[14px] capitalize text-[#12130F] ">Myriad</div>
+        <div className="text-[14px] capitalize text-[#12130F] ">
+          {currentNetworkId}
+        </div>
       </Button>
       <Menu
         anchorEl={anchorEl}
@@ -48,11 +71,16 @@ export const SwitchNetwork: React.FC<NetworkOptionProps> = ({
         open={Boolean(anchorEl)}
         onClose={handleClose}
       >
-        <MenuItem>
-          <div className="h-full w-2 bg-primary absolute left-0 rounded-r"></div>
+        <MenuItem onClick={() => handleChangeNetwork('myriad')}>
+          {currentNetworkId === 'myriad' && (
+            <div className="h-full w-2 bg-primary absolute left-0 rounded-r"></div>
+          )}
           <ListNetwork image={IcMyriad} label="Myriad" />
         </MenuItem>
-        <MenuItem>
+        <MenuItem onClick={() => handleChangeNetwork('debio')}>
+          {currentNetworkId === 'debio' && (
+            <div className="h-full w-2 bg-primary absolute left-0 rounded-r"></div>
+          )}
           <ListNetwork image={IcDebio} label="Debio" />
         </MenuItem>
       </Menu>
